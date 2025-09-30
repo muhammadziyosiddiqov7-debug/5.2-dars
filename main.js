236fa5a21ba47df681fe0e96661d8e3c9b0606e1
muhammadziyosiddiqov7-debug <muhammadziyosiddiqov7@gmail.com>
2025-09-30 16:21:55 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+let string = prompt("String typedagi malumotlarni kiriting")
+
+let number= prompt("number typedagi malumotlarni kiriting")
+
+let boolean = prompt("boolean typedagi malumotlarni kiriting")
+
+
+
+
+
+confirm(`Aniq shularmi 
+${string} 
+${number} 
+${boolean}`)
+
